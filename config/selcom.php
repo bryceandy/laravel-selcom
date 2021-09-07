796706eae65d5bcc318d31f29212a46a313712d9
bryceandy <bryceandy@rocketmail.com>
2021-09-07 15:52:26 +0300

    Add config key

--- config/selcom.php
+++ config/selcom.php
@@ -28,4 +28,17 @@ return [
    | Merchant API secret
    */
     'secret' => env('SELCOM_API_SECRET'),
+
+    /*
+   |--------------------------------------------------------------------------
+   | Selcom live status
+   |--------------------------------------------------------------------------
+   |
+   | A value that determines if you are using Selcom in test mode or live.
+   | When testing, the above values would be different in every stage.
+   |
+   | For example; the SELCOM_API_SECRET in test mode is different from
+   | SELCOM_API_SECRET in live mode. The same applies to all others.
+   */
+    'live' => env('SELCOM_IS_LIVE', false),
 ];
