b7063dcf1261d4c0b9a9f8550f28286420fa0737
bryceandy <bryceandy@rocketmail.com>
2021-09-09 17:42:48 +0300

    Fake create order responses

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,31 @@
 namespace Bryceandy\Selcom\Tests;
 
 use Bryceandy\Selcom\SelcomBaseServiceProvider;
+use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    public PromiseInterface $createOrderResponse;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->createOrderResponse = Http::response(json_decode(
+            file_get_contents(__DIR__ . '/stubs/create-order-response.json'),
+            true
+        ));
+
+        // Fake all sms requests to the API
+        Http::fake([
+            'https://apigwtest.selcommobile.com/v1/checkout/create-order-minimal' => $this->createOrderResponse,
+            'https://apigwtest.selcommobile.com/v1/checkout/create-order' => $this->createOrderResponse,
+        ]);
+    }
+
     /**
      * Define environment setup.
      *
