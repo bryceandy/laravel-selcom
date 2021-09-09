aa18762835e1b69531f85061648c4e26860f9ce7
bryceandy <bryceandy@rocketmail.com>
2021-09-09 15:04:05 +0300

    Fix property default types, exception throwing

--- src/Selcom.php
+++ src/Selcom.php
@@ -11,25 +11,16 @@ class Selcom
 {
     use HandlesCheckout;
 
-    private string $vendor;
+    private $vendor;
 
-    private string $apiKey;
+    private $apiKey;
 
-    private string $apiSecret;
+    private $apiSecret;
 
     private string $apiUrl;
 
-    /**
-     * @throws ConfigurationUnavailableException
-     */
     public function __construct()
     {
-        if (! config('selcom.vendor') || ! config('selcom.key') || ! config('selcom.secret')) {
-            throw new ConfigurationUnavailableException(
-                'Your Selcom credentials can not be empty!'
-            );
-        }
-
         $this->vendor = config('selcom.vendor');
 
         $this->apiKey = config('selcom.key');
@@ -41,12 +32,24 @@ class Selcom
         $this->apiUrl = "https://$subdomain.selcommobile.com/v1/";
     }
 
-    public function prefix(): string
+    public function prefix()
     {
         return config('selcom.prefix');
     }
 
-    private function makeRequest(string $uri, string $method, array $data = []): Response
+    /**
+     * @throws ConfigurationUnavailableException
+     */
+    private function validateConfig()
+    {
+        if (! config('selcom.vendor') || ! config('selcom.key') || ! config('selcom.secret')) {
+            throw new ConfigurationUnavailableException(
+                'Your Selcom credentials can not be empty!'
+            );
+        }
+    }
+
+    public function makeRequest(string $uri, string $method, array $data = []): Response
     {
         $fullPath = $this->apiUrl . $uri;
 
@@ -56,6 +59,8 @@ class Selcom
 
     private function getHeaders($data): array
     {
+        $this->validateConfig();
+
         date_default_timezone_set('Africa/Dar_es_Salaam');
 
         $authorization = base64_encode($this->apiKey);
@@ -77,6 +82,8 @@ class Selcom
 
     private function getDigest($data, $timestamp): string
     {
+        $this->validateConfig();
+
         $signData = "timestamp=$timestamp";
 
         if (count($data)) {
