403c294046282e32e41a756fa0d4d007212b53ec
bryceandy <bryceandy@rocketmail.com>
2021-09-09 22:14:50 +0300

    Clean up test

--- tests/Feature/Facades/SelcomFacadeTest.php
+++ tests/Feature/Facades/SelcomFacadeTest.php
@@ -5,23 +5,14 @@ namespace Bryceandy\Selcom\Tests\Feature\Facades;
 use Bryceandy\Selcom\Exceptions\MissingDataException;
 use Bryceandy\Selcom\Facades\Selcom;
 use Bryceandy\Selcom\Tests\TestCase;
-use Illuminate\Foundation\Application;
 
 class SelcomFacadeTest extends TestCase
 {
-    /**
-     * Define environment setup.
-     *
-     * @param Application $app
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('selcom.vendor', null);
-    }
-
     /** @test */
     public function test_facade_requires_configuration_to_make_requests()
     {
+        $this->app['config']->set('selcom.vendor', null);
+
         $this->expectException(MissingDataException::class);
 
         Selcom::makeRequest('', 'GET');
