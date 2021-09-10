a559cdc74c4bda287b31602d72fcf06c037ed325
bryceandy <bryceandy@rocketmail.com>
2021-09-10 15:38:13 +0300

    Add config keys

--- config/selcom.php
+++ config/selcom.php
@@ -50,4 +50,44 @@ return [
    | This prefix will be used for routes and on Selcom order IDs.
    */
     'prefix' => env('SELCOM_PREFIX', 'selcom'),
+
+    /*
+   |--------------------------------------------------------------------------
+   | Redirect URL
+   |--------------------------------------------------------------------------
+   |
+   | The URL where your users will be taken to after a payment is complete.
+   */
+    'redirect_url' => env('SELCOM_REDIRECT_URL'),
+
+    /*
+   |--------------------------------------------------------------------------
+   | Cancel URL
+   |--------------------------------------------------------------------------
+   |
+   | The URL where your users will be taken to when they cancel the payment.
+   */
+    'cancel_url' => env('SELCOM_CANCEL_URL'),
+
+    /*
+   |--------------------------------------------------------------------------
+   | Payment Gateway Colors
+   |--------------------------------------------------------------------------
+   |
+   | Colors for your payment gateway page.
+   */
+    'colors' => [
+        'header' => env('SELCOM_HEADER_COLOR', '#FF0012'),
+        'link' => env('SELCOM_LINK_COLOR', '#FF0012'),
+        'button' => env('SELCOM_BUTTON_COLOR', '#FF0012'),
+    ],
+
+    /*
+   |--------------------------------------------------------------------------
+   | Payment Expiry
+   |--------------------------------------------------------------------------
+   |
+   | Time in minutes before the payment gateway page expires.
+   */
+    'expiry' => env('SELCOM_PAYMENT_EXPIRY', 60)
 ];
