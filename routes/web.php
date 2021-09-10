3a060dff5a666faf979f8c6e06c29e44ed3af301
bryceandy <bryceandy@rocketmail.com>
2021-09-10 19:20:50 +0300

    Add route names

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('checkout-callback', CheckoutCallbackController::class)
     ->name('selcom.checkout-callback');;
 
-Route::view('redirect', 'selcom::redirect');
+Route::view('redirect', 'selcom::redirect')->name('selcom.redirect');
 
-Route::view('cancel', 'selcom::cancel');
+Route::view('cancel', 'selcom::cancel')->name('selcom.cancel');
